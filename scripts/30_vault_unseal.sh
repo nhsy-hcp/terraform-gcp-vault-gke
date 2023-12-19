231e37a89d20911f64c103f12b1a6376e7373383
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2023-12-19 10:24:33 +0000

    Updated vault unseal script

--- scripts/30_vault_unseal.sh
+++ scripts/30_vault_unseal.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
-set -o pipefail
-
-#kubectl wait --for=condition=PodScheduled pod --all --namespace vault --timeout=60s
 kubectl wait --for=jsonpath='{.status.phase}'=Running pod --all --namespace vault --timeout=5m
 
 sleep 3
 
-kubectl exec -n vault -ti vault-0 -- vault status
-kubectl exec -n vault -ti vault-0 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
-kubectl exec -n vault -ti vault-0 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
-kubectl exec -n vault -ti vault-0 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
-kubectl exec -n vault -ti vault-0 -- vault status
-sleep 3
-
-kubectl exec -n vault -ti vault-1 -- vault status
-kubectl exec -n vault -ti vault-1 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
-kubectl exec -n vault -ti vault-1 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
-kubectl exec -n vault -ti vault-1 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
-kubectl exec -n vault -ti vault-1 -- vault status
-sleep 3
-kubectl exec -n vault -ti vault-2 -- vault status
-kubectl exec -n vault -ti vault-2 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
-kubectl exec -n vault -ti vault-2 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
-kubectl exec -n vault -ti vault-2 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
-sleep 3
-kubectl exec -n vault -ti vault-2 -- vault status
+for i in {0..2}; do
+  echo "Unsealing vault-$i ..."
+  kubectl exec -n vault -ti vault-$i -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
+  kubectl exec -n vault -ti vault-$i -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
+  kubectl exec -n vault -ti vault-$i -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
+  kubectl exec -n vault -ti vault-$i -- vault status
+  sleep 3
+done
 echo
 export VAULT_TOKEN=$(cat vault-init.json | jq -r '.root_token')
 kubectl exec -n vault -ti vault-0 -- sh -c "VAULT_TOKEN=$VAULT_TOKEN vault operator raft list-peers"
