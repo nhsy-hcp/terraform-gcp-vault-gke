854b30edadd58e3b8e88771231574f8b50a9ff5a
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2023-12-19 10:12:14 +0000

    Updated vault init script

--- scripts/10_vault_init.sh
+++ scripts/10_vault_init.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-set -o pipefail
 
-#kubectl wait --for=condition=PodScheduled pod --all --namespace vault --timeout=60s
 kubectl wait --for=jsonpath='{.status.phase}'=Running pod --all --namespace vault --timeout=5m
-
 sleep 30
+echo "Initializing Vault ..."
 kubectl exec -n vault -ti vault-0 -- vault operator init -format=json | tee vault-init.json
-kubectl exec -n vault -ti vault-0 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
-kubectl exec -n vault -ti vault-0 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
-kubectl exec -n vault -ti vault-0 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
-kubectl exec -n vault -ti vault-0 -- vault status
-sleep 30
-kubectl exec -n vault -ti vault-1 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
-kubectl exec -n vault -ti vault-1 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
-kubectl exec -n vault -ti vault-1 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
-kubectl exec -n vault -ti vault-1 -- vault status
-sleep 30
-kubectl exec -n vault -ti vault-2 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
-kubectl exec -n vault -ti vault-2 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
-kubectl exec -n vault -ti vault-2 -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
-kubectl exec -n vault -ti vault-2 -- vault status
+
+for i in {0..2}; do
+  echo "Unsealing vault-$i ..."
+  kubectl exec -n vault -ti vault-$i -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
+  kubectl exec -n vault -ti vault-$i -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
+  kubectl exec -n vault -ti vault-$i -- vault operator unseal $(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
+  kubectl exec -n vault -ti vault-$i -- vault status
+  sleep 30
+done
